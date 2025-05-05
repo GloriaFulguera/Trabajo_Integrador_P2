8d9d450a7447d3c429e0d8d905e33eff3df988e1
Gloria Fulguera <gloriafulguera@gmail.com>
2025-05-04 21:29:47 -0300

    WIP data layer

---
+++ data/data_helper.py
@@ -0,0 +1,26 @@
+import json
+class data_helper:
+    def __init__(self,user,password):
+        self.user=user
+        self.password=password
+
+    def toDict(self):
+        ret={}
+        for attr in dir(self):
+            if not attr.startswith("__") and not attr.endswith("__"):
+                val=getattr(self,attr)
+                if not callable(val):
+                    ret[attr]=val
+
+    def fromDict(self,source):
+        for key in source.keys():
+            setattr(self,key,source[key])
+
+    def serialize(self,obj,name):
+        with open(name,"w") as f:
+            f.write(json.dumps(obj,indent=4))
+
+    def deserialize(self,name):
+        with open(name,"r") as f:
+            txt=f.read()
+            return json.loads(txt)
