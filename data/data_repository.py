85035f0e703f35ea1c424d6d01c51fca222dbdbe
Gloria Fulguera <gloriafulguera@gmail.com>
2025-06-21 01:55:15 -0300

    clean code

--- data/data_repository.py
+++ data/data_repository.py
@@ -1,9 +1,10 @@
 import sqlobject as SO
-from data.data_helper import data_helper
+from os import getenv
+from dotenv import load_dotenv
 import bcrypt
 
-database='mysql://guest:1234@localhost/prog2'
-__connection__=SO.connectionForURI(database)
+load_dotenv()
+__connection__=SO.connectionForURI(getenv("DATABASE"))
 
 class Usuarios(SO.SQLObject):
     usuario=SO.StringCol(length=100, varchar=True)
@@ -11,31 +12,18 @@ class Usuarios(SO.SQLObject):
     cuentas=SO.MultipleJoin('Cuentas', joinColumn='id_usuario')
 
 class UserRepository:
-    #def __init__(self):
-        #self.archivo=archivo
-        #self.dh=data_helper()
 
     def create_user(self,p_user,p_pwd):
-        #usuarios=self.dh.deserialize(self.archivo)
         hash=bcrypt.hashpw(p_pwd.encode(), bcrypt.gensalt()).decode()
-        #usuarios[p_user] = hash
-        #self.dh.serialize(usuarios,self.archivo)
         Usuarios(usuario=p_user, clave=hash)
 
-
     def user_exists(self,p_user):
-        #usuarios2=self.dh.deserialize(self.archivo)
         try:
             existe=Usuarios.selectBy(usuario=p_user)
             existe.getOne()
             return True
         except SO.SQLObjectNotFound:
             return False
-
-        # if p_user in usuarios2:
-        #     return True
-        # else:
-        #     return False
 
     def user_valid(self,p_user,p_pwd):
         user=None
@@ -45,5 +33,4 @@ class UserRepository:
             return bcrypt.checkpw(p_pwd.encode(), user.clave.encode())
         except SO.SQLObjectNotFound:
             return False
-        # usuarios=self.dh.deserialize(self.archivo)
-        # return bcrypt.checkpw(pwd.encode(),usuarios[user].encode())
+
--- data/transaction_repository.py
+++ data/transaction_repository.py
@@ -1,14 +1,12 @@
 import sqlobject as SO
-from data.data_helper import data_helper
-from os.path import exists
-from os import makedirs,getenv
+from os import getenv
 from requests import get
 from dotenv import load_dotenv
 from decimal import Decimal
 from data.data_repository import Usuarios
 
-database='mysql://guest:1234@localhost/prog2'
-__connection__=SO.connectionForURI(database)
+load_dotenv()
+__connection__=SO.connectionForURI(getenv("DATABASE"))
 
 class Monedas(SO.SQLObject):
     class sqlmeta:
@@ -26,19 +24,9 @@ class Cuentas(SO.SQLObject):
 
 
 class transactionRepository:
-    def __init__(self,user,archivo="data/rates.json"):
+    def __init__(self,user):
         self.user=user
-        # self.ratesFile=archivo
-        self.dh=data_helper()
-        # self.userFile='data/accounts/{}.json'.format(self.user)
-
-        # if not exists("data/accounts"):
-        #     makedirs("data/accounts")
-
-        # if not exists(self.userFile):
-        #     self.dh.serialize({"ARS":"0.00"},self.userFile)
         self.create_account_default()
-        load_dotenv()
 
     def get_rates(self):
         rates=None
@@ -46,8 +34,6 @@ class transactionRepository:
         if response.status_code==200:
             data=response.json()
             rates=data["rates"]
-            # if not exists(self.ratesFile):
-            #     self.dh.serialize(rates,self.ratesFile)
 
         return rates
     
@@ -60,15 +46,12 @@ class transactionRepository:
             Cuentas(id_usuario=user.id, id_moneda=curr.codigo, saldo=0)
 
     def create_account(self,p_curr):
-        # accs=self.dh.deserialize(self.userFile)
-        # accs[curr]="0.00"
-        # self.dh.serialize(accs,self.userFile)
         try:
             user=Usuarios.selectBy(usuario=self.user).getOne()
             curr=Monedas.selectBy(codigo=p_curr).getOne()
             Cuentas(id_usuario=user.id, id_moneda=curr.codigo, saldo=0)
         except SO.SQLObjectNotFound:
-            raise ValueError("Ocurrio un error, contacte al administrador")
+            raise ValueError("Ocurrio un error en create_account, contacte al administrador")
 
     def account_exists(self,p_curr):
         try:
@@ -77,11 +60,6 @@ class transactionRepository:
             return True
         except SO.SQLObjectNotFound:
             return False
-        # accs=self.dh.deserialize(self.userFile)
-        # if curr in accs:
-        #     return True
-        # else:
-        #     return False
         
     def rate_exists(self,p_rate):
         try:
@@ -89,13 +67,6 @@ class transactionRepository:
             return True
         except SO.SQLObjectNotFound:
             return False
-        # if not exists(self.ratesFile):
-        #     self.get_rates()
-        # rates=self.dh.deserialize(self.ratesFile)
-        # if rate in rates:
-        #     return True
-        # else:
-        #     return False
         
     def credit_account(self,p_amt,p_acc):
         try:
@@ -104,11 +75,7 @@ class transactionRepository:
             value= acc.saldo+p_amt
             acc.saldo=value
         except SO.SQLObjectNotFound:
-            raise ValueError("credit_account:Ocurrio un error, contacte al administrador")
-        # accounts=self.dh.deserialize(self.userFile)
-        # value= Decimal(accounts[acc])+amt
-        # accounts[acc]=str(value)
-        # self.dh.serialize(accounts,self.userFile)
+            raise ValueError("Ocurrio un error en credit_account, contacte al administrador")
 
     def get_balance(self,p_acc):
         try:
@@ -116,9 +83,7 @@ class transactionRepository:
             acc=Cuentas.select(SO.AND(Cuentas.q.id_usuario==user.id,Cuentas.q.id_moneda==p_acc)).getOne()
             return acc.saldo
         except SO.SQLObjectNotFound:
-            raise ValueError("get_balance:Ocurrio un error, contacte al administrador")
-        #accounts=self.dh.deserialize(self.userFile)
-        #return Decimal(accounts[acc])
+            raise ValueError("Ocurrio un error en get_balance, contacte al administrador")
     
     def get_rate(self,curr,rates):
         return Decimal(rates[curr])
@@ -134,11 +99,5 @@ class transactionRepository:
             accountOrg.saldo=discount
             accountDst.saldo=credit
         except SO.SQLObjectNotFound:
-            raise ValueError("complete_exchange:Ocurrio un error, contacte al administrador")
-        # accounts=self.dh.deserialize(self.userFile)
-        # discount=Decimal(accounts[org])-Decimal(withdraw)
-        # credit=Decimal(accounts[dst])+Decimal(deposit)
-        # accounts[org]=str(discount)
-        # accounts[dst]=str(credit)
+            raise ValueError("Ocurrio un error en complete_exchange, contacte al administrador")
 
-        # self.dh.serialize(accounts,self.userFile)
