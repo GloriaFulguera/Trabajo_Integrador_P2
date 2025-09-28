75c170b22353c2f9e38df999658c3d145c7a8567
Gloria Fulguera <gloriafulguera@gmail.com>
2025-09-27 22:02:05 -0300

    funcionalidad pantalla ppal

--- presentation/main_window.py
+++ presentation/main_window.py
@@ -1,7 +1,43 @@
 from PyQt6.QtWidgets import QMainWindow
-from presentation.screens.MainWindow import Ui_MainWindow
+from PyQt6.QtCore import pyqtSignal
+from presentation.screens.MainWindow_ui import Ui_MainWindow
 
 class MainWindow(QMainWindow,Ui_MainWindow):
-    def __init__(self):
+    logoutRequested=pyqtSignal()
+
+    def __init__(self,user):
         super().__init__()
         self.setupUi(self)
+        self.lblWelcomeUser.setText("Sesión de "+user)
+        self.btnCerrar.clicked.connect(self.logout)
+        self.user=user
+        self.btnCreateAccountM.clicked.connect(self.showCreateAccount)
+        self.btnPurchaseM.clicked.connect(self.showPurchase)
+        self.btnSellM.clicked.connect(self.showSell)
+        self.btnDepositM.clicked.connect(self.showDeposit)
+
+
+    def validateEmpty(self,value):
+        if str(value).strip() == "":
+            raise ValueError("Debe completar todos los campos.")
+        
+    def cleanInputs(self):
+        self.txtAmount.clear()
+        self.txtPurchaseAmount.clear()
+        self.txtSellAmount.clear()
+
+    def logout(self):
+        self.hide()
+        self.logoutRequested.emit()
+
+    def showCreateAccount(self):
+        self.stackMain.setCurrentIndex(0)
+
+    def showPurchase(self):
+        self.stackMain.setCurrentIndex(1)
+
+    def showSell(self):
+        self.stackMain.setCurrentIndex(2)
+
+    def showDeposit(self):
+        self.stackMain.setCurrentIndex(3)
